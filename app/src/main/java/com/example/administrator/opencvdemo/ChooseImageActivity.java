25cca19a7515ce5af86bb538a0bc8954aaadffeb
wuzuqing <wuzuqing@linghit.com>
2019-09-16 18:39:58 +0800

    增加对应页面的拦截

---
+++ app/src/main/java/com/example/administrator/opencvdemo/ChooseImageActivity.java
@@ -0,0 +1,47 @@
+package com.example.administrator.opencvdemo;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+
+/**
+ * 作者：士元
+ * 时间：2019/9/16 18:30
+ * 邮箱：wuzuqing@linghit.com
+ * 说明：
+ */
+public class ChooseImageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_image);
+
+    }
+    public static final int RC_CHOOSE_PHOTO = 2;
+
+    private void choosePhoto() {
+        Intent intentToPickPic = new Intent(Intent.ACTION_PICK, null);
+        intentToPickPic.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+        startActivityForResult(intentToPickPic, RC_CHOOSE_PHOTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RC_CHOOSE_PHOTO:
+                Uri uri = data.getData();
+                // String filePath = FileUtil.getFilePathByUri(this, uri);
+                // if (!TextUtils.isEmpty(filePath)) {
+                //
+                // }
+                break;
+        }
+    }
+
+}
